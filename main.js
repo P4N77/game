7532a9d6255beda64739fc4e3231b9cfd0b34cd7
German <germanm1853@gmail.com>
2022-09-16 17:00:31 -0500

    piedra papel o tijera funcionando

---
+++ main.js
@@ -0,0 +1,59 @@
+let partidas=+prompt("por favor ingrese la cantidad de partidas que desea jugar")
+let juego
+let juegoPc
+let contPc=0
+let contUser=0
+let contEmpate=0
+for (let i = 0; i <partidas; i++) {
+    juegoPc=Math.floor(Math.random()*3+1)
+
+    juego=+prompt("por favor escoja una de las opciones:\n 1:Piedra. \n 2:Papel. \n 3:Tijera")
+
+    if(juego==1 && juegoPc==2){
+        alert("PC Win! \n jugador eligio:Piedra \n PC eligio:Papel")
+        contPc=contPc+1
+    }
+
+    if(juego==1 && juegoPc==3){
+        alert("User Win! \n jugador eligio:Piedra \n PC eligio:Tijera")
+        contUser=contUser+1
+    }
+
+    if(juego==1 && juegoPc==1){
+        alert("Draw! \n jugador eligio:Piedra \n PC eligio:Piedra")
+        contEmpate=contEmpate+1
+    }
+
+    if(juego==2 && juegoPc==2){
+        alert("Draw! \n jugador eligio:Papel \n PC eligio:Papel")
+        contEmpate=contEmpate+1
+    }
+
+    if(juego==2 && juegoPc==3){
+        alert("PC Win! \n jugador eligio:Papel \n PC eligio:Tijera")
+        contPc=contPc+1
+    }
+
+    if(juego==2 && juegoPc==1){
+        alert("User Win! \n jugador eligio:Papel \n PC eligio:Piedra")
+        contUser=contUser+1
+    }
+
+    if(juego==3 && juegoPc==2){
+        alert("User Win! \n jugador eligio:Tijera \n PC eligio:Papel")
+        contUser=contUser+1
+    }
+
+    if(juego==3 && juegoPc==3){
+        alert("Draw! \n jugador eligio:Tijera \n PC eligio:Tijera")
+        contEmpate=contEmpate+1
+    }
+
+    if(juego==3 && juegoPc==1){
+        alert("PC Win! \n jugador eligio:Tijera \n PC eligio:Piedra")
+        contPc=contPc+1
+    }
+
+}
+
+alert("Fin del Juego. \n\n Resultados:\n User: "+contUser+" Victoria(s) \n PC: "+contPc+" Victoria(s) \n Empates: "+contEmpate+" Empate(s)")
